07aeef9a895f2e4a4bb98b1bd8a16e0f3266354c
Gustav Ahlberg <Gustav.Ahlberg@gmail.com>
2014-02-19 09:44:21 +0100

    Don't break if fetching users fail

--- public/backend/users.php
+++ public/backend/users.php
@@ -2,16 +2,21 @@
 
 require_once(__DIR__ . "/../../PlexWWWatch.php");
 
-try {
-    $plexWWWatch = new PlexWWWatch();
-    if (isset($_GET["user"])) {
-        $username = $_GET["user"];
-        echo json_encode($plexWWWatch->plexWatch()->user($username), JSON_PRETTY_PRINT);
-    } else {
-        echo json_encode($plexWWWatch->plexWatch()->users());
+$plexWWWatch = new PlexWWWatch();
+
+if (isset($_GET["user"])) {
+    $username = $_GET["user"];
+    try {
+        echo json_encode($plexWWWatch->plexWatch()->user($username));
+    } catch (Exception $e) {
+        echo "{watched: []}";
     }
-} catch (Exception $e) {
-    echo "[]";
+} else {
+    try {
+        echo json_encode($plexWWWatch->plexWatch()->users());
+    }  catch (Exception $e) {
+        echo "[]";
+    }
 }
 
 
